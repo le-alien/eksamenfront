1bdb839e554f21bebad477a963774c263313974b
CyberPizza <your@example.com>
2024-05-15 07:53:31 +0200

    homepage improvements

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navigationbar from "../components/Navigationbar.tsx"
 import "../index.css"
 import Headerxl from "../components/Headerxl.tsx";
@@ -19,10 +19,76 @@ export default () => {
                     Forum
                 </span>
             </div>
-            <div className="bg-cloudWhite">
-                <Headerxl>
-                    SomethingForum boards
-                </Headerxl>
+            <div className="flex justify-center">
+                <div className="w-2/3">
+                    <div className="bg-lightBrownMountain border-solid border-2 border-cloudWhite my-2 py-0.5">
+                        <div className="flex mx-2 content-center">    
+                            <Headerxl>
+                                SomethingForum boards
+                            </Headerxl>
+                        </div>
+                    </div>
+                    <div className="flex bg-lightBrownMountain border-cloudWhite flex-row items-start">
+                        <a href="/GeneralBoard" className="flex">
+                            <span className="content-center">
+                                <img className="w-14 h-4/5 mt-1 ml-1" src="src\assets\mellpond.gif" alt="" />
+                            </span>
+                            <div className="flex flex-col mx-2">
+                                <Headerxl>
+                                    General Discussion
+                                </Headerxl>
+                                <span>
+                                    This board is for all and any discussion
+                                </span>
+                           </div>
+                        </a>
+                    </div>
+                    <div className="flex bg-lightBrownMountain border-cloudWhite flex-row items-start">
+                        <a href="/CurrentBoard" className="flex">
+                            <span className="content-center">
+                                <img className="w-14 h-4/5 mt-1 ml-1" src="src\assets\mellpond.gif" alt="" />
+                            </span>
+                            <div className="flex flex-col mx-2">
+                                <Headerxl>
+                                    Current Discussion
+                                </Headerxl>
+                                <span>
+                                    This board is for discussion of current topics
+                                </span>
+                           </div>
+                        </a>
+                    </div>
+                    <div className="flex bg-lightBrownMountain border-cloudWhite flex-row items-start">
+                        <a href="/CurrentBoard" className="flex">
+                            <span className="content-center">
+                                <img className="w-14 h-4/5 mt-1 ml-1" src="src\assets\mellpond.gif" alt="" />
+                            </span>
+                            <div className="flex flex-col mx-2">
+                                <Headerxl>
+                                    Music Discussion
+                                </Headerxl>
+                                <span>
+                                    This board is for discussion of music
+                                </span>
+                           </div>
+                        </a>
+                    </div>
+                    <div className="flex bg-lightBrownMountain border-cloudWhite flex-row items-start">
+                        <a href="/CurrentBoard" className="flex">
+                            <span className="content-center">
+                                <img className="w-14 h-4/5 mt-1 ml-1" src="src\assets\mellpond.gif" alt="" />
+                            </span>
+                            <div className="flex flex-col mx-2">
+                                <Headerxl>
+                                    Dwarf Fortress Discussion
+                                </Headerxl>
+                                <span>
+                                    This board is for discussion of Dwarf Fortress
+                                </span>
+                           </div>
+                        </a>
+                    </div>
+                </div>
             </div>
 
 
